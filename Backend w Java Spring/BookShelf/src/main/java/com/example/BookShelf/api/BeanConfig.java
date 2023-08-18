4621d82b195b510897b34ba1bdf162b85537c3e0
Taha Baran <tahabaranakti10@gmail.com>
2023-08-18 10:50:40 +0300

    Security implamantation contiunes

--- Backend w Java Spring/BookShelf/src/main/java/com/example/BookShelf/api/BeanConfig.java
+++ Backend w Java Spring/BookShelf/src/main/java/com/example/BookShelf/api/BeanConfig.java
@@ -1,5 +1,6 @@
 package com.example.BookShelf.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,4 +12,7 @@ public class BeanConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public ObjectMapper objectMapper(){ return new ObjectMapper(); }
 }
--- Backend w Java Spring/BookShelf/src/main/java/com/example/BookShelf/configure/ServiceConfig.java
+++ Backend w Java Spring/BookShelf/src/main/java/com/example/BookShelf/configure/ServiceConfig.java
@@ -1,4 +1,65 @@
 package com.example.BookShelf.configure;
 
+import Security.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class ServiceConfig {
+
+    private final JwtFilter jwtFilter;
+
+    public ServiceConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .AntMatchers("/api/admin").hasAuthority("ADMIN")
+                .AntMatchers("/api/user").hasAnyAuthority("USER", "ADMIN")
+                .AnyRequest().authenticated()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .and().build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers("/api/images/**", "/api/auth/login"));
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
